921897c813b7221e33f66b7a8a3cf8d1c2d8d27b
zackery51996 <zlkim@csustudent.net>
2017-04-24 12:54:42 -0400

    CheckersBoard

--- src/Main.java
+++ src/Main.java
@@ -12,8 +12,6 @@ public class Main {
     public static void main(String[] args) {
         Main m = new Main();
         m.gameLoop();
-
-        System.out.println("Congratulations to the Winner and to Both Players on a Successful Game of Checkers!");
     }
 
     public void gameLoop() {
@@ -21,27 +19,22 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         int xPiece; int yPiece; int xMove; int yMove;
 
-        // Set the Board
-        board.resetBoard();
-
         while (gameContinue(u1Pieces, u2Pieces)) {
-
             // Display the Set Board to Begin the Game
             board.printBoard();
 
             // Get Input from the User for their Move
             System.out.println("Please Enter The X-Coordinate of the Piece You Would Like to Move: ");
-            xPiece = scanner.nextInt() - 1;
+            xPiece = scanner.nextInt();
             System.out.println("Please Enter The Y-Coordinate of the Piece You Would Like to Move: ");
-            yPiece = scanner.nextInt() - 1;
+            yPiece = scanner.nextInt();
             System.out.println("Please Enter The X-Coordinate of Where You Would Like to Move the Piece: ");
-            xMove = scanner.nextInt() - 1;
+            xMove = scanner.nextInt();
             System.out.println("Please Enter The Y-Coordinate of Where You Would Like to Move the Piece: ");
-            yMove = scanner.nextInt() - 1;
+            yMove = scanner.nextInt();
 
             // Check to See if the Move is Valid, and continue asking until valid input is given
             while (!isValid(user, xPiece, yPiece, xMove, yMove, board)) {
-                System.out.println("Invalid. Please Enter New Locations.");
                 System.out.println("Please Enter The X-Coordinate of the Piece You Would Like to Move: ");
                 xPiece = scanner.nextInt();
                 System.out.println("Please Enter The Y-Coordinate of the Piece You Would Like to Move: ");
@@ -54,29 +47,6 @@ public class Main {
 
             // Move the piece to the desired location
             board.movePiece(xPiece, yPiece, xMove, yMove);
-
-            if (user == 1) {
-                if (xMove == 0) {
-                    board.mBoard[xMove][yMove] = new King();
-                    board.mBoard[xMove][yMove].setPiece('X');
-                }
-            }
-            else if (user == 2) {
-                if (xMove == 7) {
-                    board.mBoard[xMove][yMove] = new King();
-                    board.mBoard[xMove][yMove].setPiece('O');
-                }
-            }
-
-            // Decrement the place holding variables for the amount of pieces each player has if a jump was made
-            if (Math.abs(xMove - xPiece) == 2 && Math.abs(yMove - yPiece) == 2) {
-                if (user == 1) {
-                    u2Pieces -= 1;
-                }
-                else if (user == 2) {
-                    u1Pieces -= 1;
-                }
-            }
 
             // Change the Current User Variable to the Next User
             if (user == 1) {
@@ -93,8 +63,7 @@ public class Main {
             return false;
         }
 
-        if (Math.abs(xMove - xPiece) > 2 || Math.abs(yMove - yPiece) > 2 ||
-                Math.abs(xMove - xPiece) < 1 || Math.abs(yMove - yPiece) < 1) {
+        if (Math.abs(xMove - xPiece) > 2 || Math.abs(yMove - yPiece) > 2 || Math.abs(xMove - xPiece) < 1 || Math.abs(yMove - yPiece) < 1) {
             return false;
         }
 
@@ -109,62 +78,17 @@ public class Main {
                 else if (xMove - xPiece == -2 && Math.abs(yMove - yPiece) == 2) {
                     if (board.mBoard[xMove][yMove].getPiece() == '.') {
                         if (yMove - yPiece == -2) {
-                            if (board.mBoard[xMove + 1][yMove + 1].getPiece() == 'o' ||
-                                    board.mBoard[xMove + 1][yMove + 1].getPiece() == 'O') {
+                            if (board.mBoard[xMove + 1][yMove + 1].getPiece() == 'o') {
                                 return true;
                             }
                             else { return false; }
                         }
                         else if (yMove - yPiece == 2) {
-                            if (board.mBoard[xMove + 1][yMove - 1].getPiece() == 'o' ||
-                                    board.mBoard[xMove + 1][yMove - 1].getPiece() == 'O') {
+                            if (board.mBoard[xMove + 1][yMove - 1].getPiece() == 'o') {
                                 return true;
                             }
                             else { return false; }
                         }
-                    }
-                    else { return false; }
-                }
-                else { return false; }
-            }
-            if (board.mBoard[xPiece][yPiece].getPiece() == 'X') {
-                if (Math.abs(xMove - xPiece) == 1 && Math.abs(yMove - yPiece) == 1) {
-                    if (board.mBoard[xMove][yMove].getPiece() == '.') {
-                        return true;
-                    }
-                    else { return false; }
-                }
-                else if (Math.abs(xMove - xPiece) == 2 && Math.abs(yMove - yPiece) == 2) {
-                    if (board.mBoard[xMove][yMove].getPiece() == '.') {
-                        if (xMove - xPiece == -2 && yMove - yPiece == -2) {
-                            if (board.mBoard[xMove + 1][yMove + 1].getPiece() == 'o' ||
-                                    board.mBoard[xMove + 1][yMove + 1].getPiece() == 'O') {
-                                return true;
-                            }
-                            else { return false; }
-                        }
-                        else if (xMove - xPiece == -2 && yMove - yPiece == 2) {
-                            if (board.mBoard[xMove + 1][yMove - 1].getPiece() == 'o' ||
-                                    board.mBoard[xMove + 1][yMove - 1].getPiece() == 'O') {
-                                return true;
-                            }
-                            else { return false; }
-                        }
-                        else if (xMove - xPiece == 2 && yMove - yPiece == 2) {
-                            if (board.mBoard[xMove - 1][yMove - 1].getPiece() == 'o' ||
-                                    board.mBoard[xMove - 1][yMove - 1].getPiece() == 'O') {
-                                return true;
-                            }
-                            else { return false; }
-                        }
-                        else if (xMove - xPiece == 2 && yMove - yPiece == -2) {
-                            if (board.mBoard[xMove - 1][yMove + 1].getPiece() == 'o' ||
-                                    board.mBoard[xMove - 1][yMove + 1].getPiece() == 'O') {
-                                return true;
-                            }
-                            else { return false; }
-                        }
-                        else { return false; }
                     }
                     else { return false; }
                 }
@@ -183,15 +107,13 @@ public class Main {
                 else if (xMove - xPiece == 2 && Math.abs(yMove - yPiece) == 2) {
                     if (board.mBoard[xMove][yMove].getPiece() == '.') {
                         if (yMove - yPiece == -2) {
-                            if (board.mBoard[xMove - 1][yMove + 1].getPiece() == 'x' ||
-                                    board.mBoard[xMove - 1][yMove + 1].getPiece() == 'X') {
+                            if (board.mBoard[xMove - 1][yMove + 1].getPiece() == 'x') {
                                 return true;
                             }
                             else { return false; }
                         }
                         else if (yMove - yPiece == 2) {
-                            if (board.mBoard[xMove - 1][yMove - 1].getPiece() == 'x' ||
-                                    board.mBoard[xMove - 1][yMove - 1].getPiece() == 'X') {
+                            if (board.mBoard[xMove - 1][yMove - 1].getPiece() == 'x') {
                                 return true;
                             }
                             else { return false; }
@@ -199,49 +121,6 @@ public class Main {
                     }
                     else { return false; }
                 }
-            }
-            else if (board.mBoard[xPiece][yPiece].getPiece() == 'O') {
-                if (Math.abs(xMove - xPiece) == 1 && Math.abs(yMove - yPiece) == 1) {
-                    if (board.mBoard[xMove][yMove].getPiece() == '.') {
-                        return true;
-                    }
-                    else { return false; }
-                }
-                else if (Math.abs(xMove - xPiece) == 2 && Math.abs(yMove - yPiece) == 2) {
-                    if (board.mBoard[xMove][yMove].getPiece() == '.') {
-                        if (xMove - xPiece == -2 && yMove - yPiece == -2) {
-                            if (board.mBoard[xMove + 1][yMove + 1].getPiece() == 'x' ||
-                                    board.mBoard[xMove + 1][yMove + 1].getPiece() == 'X') {
-                                return true;
-                            }
-                            else { return false; }
-                        }
-                        else if (xMove - xPiece == -2 && yMove - yPiece == 2) {
-                            if (board.mBoard[xMove + 1][yMove - 1].getPiece() == 'x' ||
-                                    board.mBoard[xMove + 1][yMove - 1].getPiece() == 'X') {
-                                return true;
-                            }
-                            else { return false; }
-                        }
-                        else if (xMove - xPiece == 2 && yMove - yPiece == 2) {
-                            if (board.mBoard[xMove - 1][yMove - 1].getPiece() == 'x' ||
-                                    board.mBoard[xMove - 1][yMove - 1].getPiece() == 'X') {
-                                return true;
-                            }
-                            else { return false; }
-                        }
-                        else if (xMove - xPiece == 2 && yMove - yPiece == -2) {
-                            if (board.mBoard[xMove - 1][yMove + 1].getPiece() == 'x' ||
-                                    board.mBoard[xMove - 1][yMove + 1].getPiece() == 'X') {
-                                return true;
-                            }
-                            else { return false; }
-                        }
-                        else { return false; }
-                    }
-                    else { return false; }
-                }
-
             }
             else { return false; }
         }
